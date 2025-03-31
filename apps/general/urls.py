f53cece8cac1fdd2b2320b70d5fa423478f81459
elMauro2003 <mauricioelanimal2003@gmail.com>
2025-03-31 15:31:13 -0400

    feat[testing]: test template and text_input component

--- apps/general/urls.py
+++ apps/general/urls.py
@@ -1,7 +1,8 @@
-from apps.general.views import index
+from apps.general.views import index, test
 from django.urls import path, include, re_path
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('test', test, name='test'),
 ]
