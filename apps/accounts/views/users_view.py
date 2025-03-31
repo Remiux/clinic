6c2c967ce1362f010e54be283cd1a8002e08be0e
mangelryujin <mangelryujin@gmail.com>
2025-03-31 18:52:14 -0400

    feat[authentication]: Create authentication proccess

--- apps/accounts/views/users_view.py
+++ apps/accounts/views/users_view.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from apps.accounts.models import User 
 
 # Create your views here.
-
+@login_required(login_url='/login')
 def users_view(request):
-    return render(request,'pages/accounts/index.html')
+    context={
+        "users":User.objects.all()
+    }
+    return render(request,'pages/accounts/index.html',context)
