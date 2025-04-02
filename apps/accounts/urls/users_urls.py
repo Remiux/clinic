676533eda1cc0c6e9f82eb76d67926dc316c76cb
mangelryujin <mangelryujin@gmail.com>
2025-04-02 17:11:44 -0400

    feat[user_crud]: add filters and create actions for user crud

--- apps/accounts/urls/users_urls.py
+++ apps/accounts/urls/users_urls.py
@@ -1,9 +1,11 @@
-from apps.accounts.views.users_view import users_view,update_user_view,detail_user_view
+from apps.accounts.views.users_view import users_view,update_user_view,detail_user_view,filter_users_view,create_user_view
 from django.urls import path
 
 
 urlpatterns = [
     path('users', users_view, name='users_view'),
+    path('filter-users', filter_users_view, name='filter_users_view'),
+    path('users-create/', create_user_view, name='create_user_view'),
     path('users-detail/<int:pk>/', detail_user_view, name='detail_user_view'),
     path('users-update/<int:pk>/', update_user_view, name='update_user_view'),
 ]
