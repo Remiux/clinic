05511d4f1b912b56e839d759a40d38f0556df82f
mangelryujin <mangelryujin@gmail.com>
2025-04-09 00:08:41 -0400

    feat[customerCreate]: add customer create form UX/UI

--- apps/symptom/models.py
+++ apps/symptom/models.py
@@ -6,6 +6,20 @@ import uuid
 
 # Create your models here.
 
+class Diagnostic(models.Model):
+    code = models.CharField(max_length=15, unique=True)
+    description = models.TextField(max_length=500)
+    
+
+    class Meta:
+        verbose_name = "Diagnostic"
+        verbose_name_plural = "Diagnostics"
+
+    def __str__(self):
+        return self.code
+
+    
+    
 class Symptom(models.Model):
     code = models.CharField(max_length=255, unique=True)
     description = models.TextField(max_length=1000)
@@ -46,16 +60,47 @@ class EncryptedFile(models.Model):
         return f"{self.file.name} - {self.uploaded_by.username}"
 
 class Client(models.Model):
-    name = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    address = models.TextField(max_length=500)
-    code = models.CharField(max_length=12, unique=True, editable=False)
-
+    case_no = models.CharField(max_length=40, unique=True,default='FS0001')
+    first_name = models.CharField(max_length=255,default='John')
+    last_name = models.CharField(max_length=255,default='Doe')
+    email = models.EmailField(unique=True,default='example@gmail.com')
+    address = models.TextField(max_length=500,default='123 Main St, Springfield')
+    contact_no = models.CharField(max_length=20,default='1234567890')
+    home_phone = models.CharField(max_length=20, null=True, blank=True)
+    movile = models.CharField(max_length=20,default='1234567890')
+    city = models.CharField(max_length=50,default=' Springfield')
+    state = models.CharField(max_length=50,default='IL')
+    zip = models.CharField(max_length=50,default='62701')
+    dob = models.DateField(default=timezone.now)
+    gerder = models.CharField(max_length=50, default='M', choices=[('M', 'MALE'), ('F', 'FAMALE')])
+    ssn = models.CharField(max_length=50, unique=True,default='1234567890')
+    mma = models.CharField(max_length=50, null=True, blank=True)
+    medicaid_no = models.CharField(max_length=50, unique=True,default='1234567890')
+    medicare_no = models.CharField(max_length=50, null=True, blank=True)
+    school = models.CharField(max_length=50, null=True, blank=True)
+    grade = models.CharField(max_length=50, null=True, blank=True)
+    race = models.CharField(max_length=50,default='W' ,choices=[('W', 'White'), ('AAB', 'African American Black'), ('API', 'Asian or Other Pacific Islander'), ('AIAN', 'American Indian-Alaskan Native'), ('O', 'Other')])
+    ethnicity = models.CharField(max_length=50,default='H',choices=[('H', 'Hispanic-latino'), ('NH', 'Non-Hispanic'), ('O', 'Other')])
+    legal_guardian_full_name = models.CharField(max_length=80, null=True, blank=True)
+    legal_guardian_relationship = models.CharField(max_length=80, null=True, blank=True)
+    legal_guardian_phone_number = models.CharField(max_length=80, null=True, blank=True)
+    legal_guardian_address = models.CharField(max_length=150, null=True, blank=True)
+    legal_guardian_city = models.CharField(max_length=80, null=True, blank=True)
+    legal_guardian_state = models.CharField(max_length=80, null=True, blank=True)
+    legal_guardian_zip = models.CharField(max_length=80, null=True, blank=True)
+    emergency_contact_person = models.CharField(max_length=80,default='John Doe')
+    emergency_contact_relationship = models.CharField(max_length=80,default='wife')
+    emergency_contact_phone_number = models.CharField(max_length=20,default='1234567890')
+    insurance = models.ForeignKey(Insurance, on_delete=models.CASCADE, related_name='insurances_client')
+    diagnostic = models.ForeignKey(Diagnostic, on_delete=models.CASCADE, related_name='diagnostic_client')
+    diagnostic_two = models.ForeignKey(Diagnostic, on_delete=models.CASCADE,null=True,blank=True, related_name='diagnostic_two_client')
+    diagnostic_three = models.ForeignKey(Diagnostic, on_delete=models.CASCADE,null=True,blank=True, related_name='diagnostic_three_client')
+    
+    
+    
     def save(self, *args, **kwargs):
-        if not self.code:
-            self.code = uuid.uuid4().hex[:12].upper()  # Genera un código alfanumérico de 12 caracteres
+        # if not self.code:
+        #     self.code = uuid.uuid4().hex[:12].upper()  # Genera un código alfanumérico de 12 caracteres
         super(Client, self).save(*args, **kwargs)
 
     class Meta:
@@ -65,17 +110,10 @@ class Client(models.Model):
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
-
-class Diagnostic(models.Model):
-    code = models.CharField(max_length=15, unique=True)
-    description = models.TextField(max_length=500)
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name }"
     
-
-    class Meta:
-        verbose_name = "Diagnostic"
-        verbose_name_plural = "Diagnostics"
-
-    def __str__(self):
-        return self.name
-
-    
+    @property
+    def in_active(self):
+        return True
--- apps/symptom/views/customers_view.py
+++ apps/symptom/views/customers_view.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from apps.symptom.filters import InsuranceFilter
+from apps.symptom.filters import ClientFilter, InsuranceFilter
 from apps.symptom.form import InsuranceForm
-from apps.symptom.models import Insurance, Symptom
+from apps.symptom.models import Client, Diagnostic, Insurance, Symptom
 from utils.paginator import _get_paginator 
 
 
 # Create your views here.
 @login_required(login_url='/login')
 def customers_view(request):
-    context={}
+    context=_show_customers_filter(request)
+    context['diagnostics'] = Diagnostic.objects.all()
     return render(request,'pages/customers/index.html',context)
+
+
+@login_required(login_url='/login')
+def filter_customers_view(request):
+    context=_show_customers_filter(request)
+    return render(request,'pages/customers/customerCardList.html',context)
+
+def _show_customers_filter(request):
+    get_copy = request.GET.copy()
+    parameters = get_copy.pop('page', True) and get_copy.urlencode()
+    customers = ClientFilter(request.GET, queryset=Client.objects.all().order_by('case_no'))
+    context = _get_paginator(request, customers.qs)
+    context['parameters'] = parameters
+    return context
+
+
+@login_required(login_url='/login')
+def create_customer_view(request):
+    context={
+        'diagnostics':Diagnostic.objects.all(),
+        'insurances':Insurance.objects.filter(available=True),
+    }
+    return render(request,'pages/customers/actions/create/customerCreate.html',context)
 
 
 @login_required(login_url='/login')
