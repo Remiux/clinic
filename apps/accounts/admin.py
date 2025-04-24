861b57be4cfbf1e287befbbb61e8493360e438e7
elMauro2003 <mauricioelanimal2003@gmail.com>
2025-04-24 01:50:19 -0400

    feat[files_management]: add file management feature for users

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from apps.accounts.models import User
+from apps.symptom.models import Customer, EncryptedFileUser
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(EncryptedFileUser)
